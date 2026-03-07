a5ccffde6772a57deeba2a037525f926acb0eb15
geovanoym-oss <geovanoym@gmail.com>
2026-03-06 23:31:45 -0400

    Estructura inicial: Aqui cree el  bucle principal y la visualización del mapa

---
+++ main.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+from scripts.mapa import MAP_DATA, TAMANO_CELDA
+
+def test_mapa():
+    pygame.init()
+    # aqui hice una ventana normal, no pantalla completa aún
+    pantalla = pygame.display.set_mode((1280, 720))
+    reloj = pygame.time.Clock()
+    
+    while True:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT: pygame.quit(); sys.exit()
+        
+        pantalla.fill((0,0,0))
+        # Dibuje solo los muros para probar la estructura
+        for f, fila in enumerate(MAP_DATA):
+            for c, val in enumerate(fila):
+                if val == 1:
+                    pygame.draw.rect(pantalla, (50,50,50), (c*TAMANO_CELDA, f*TAMANO_CELDA, TAMANO_CELDA, TAMANO_CELDA))
+        
+        pygame.display.flip()
+        reloj.tick(60)
+
+if __name__ == "__main__":
+    test_mapa()
